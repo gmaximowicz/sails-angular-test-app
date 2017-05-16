50563163607585b020e8ef02b3b75cd2b391de0f
maxi <angelus016@gmail.com>
2017-05-16 10:28:40 -0300

    fix issues in images

--- assets/app/app.component.ts
+++ assets/app/app.component.ts
@@ -1,6 +1,6 @@
 import {Component} from '@angular/core';
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular/Sails App</h1>'
+    template: '<h1>My First Angular/Sails/Docker Applicattion maxi test</h1>'
 })
 export class AppComponent { }
